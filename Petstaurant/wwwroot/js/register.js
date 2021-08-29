152ecb4231e0ccab233c8189e13316560ec66a56
Shimon Cohen <shimoco7@cs.colman.ac.il>
2021-08-29 19:15:08 +0300

    updated SLN

--- Petstaurant/wwwroot/js/register.js
+++ Petstaurant/wwwroot/js/register.js
@@ -1,1 +1,12 @@
-﻿
+﻿$(function () {
+    $('form').submit(function (e) {
+        var f = $('#foo').val();
+        var msg = $('#error_message');
+
+        if (f.length == 0) {
+            e.preventDefault();
+
+            msg.text('The field foo cannot be empty');
+        }
+    });
+});
