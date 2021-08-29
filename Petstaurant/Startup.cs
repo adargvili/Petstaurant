02813c7f478e212a44d1ce970931544e13917ac6
Shimon Cohen <shimoco7@cs.colman.ac.il>
2021-08-29 19:24:40 +0300

    Shimon: Update

--- Petstaurant/Startup.cs
+++ Petstaurant/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿//Startup.cs
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
--- Petstaurant/wwwroot/js/register.js
+++ Petstaurant/wwwroot/js/register.js
@@ -1,9 +1,10 @@
 ﻿$(function () {
     $('form').submit(function (e) {
-        var f = $('#foo').val();
+        var f = $('#bDate').val();
+        console.log(f);
         var msg = $('#error_message');
 
-        if (f.length == 0) {
+        if (f.length > 0) {
             e.preventDefault();
 
             msg.text('The field foo cannot be empty');
