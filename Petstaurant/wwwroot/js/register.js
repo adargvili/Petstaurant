524705f6bba17ad7a0daa1b8f3bf4e82d41ece2c
Shimon Cohen <shimoco7@cs.colman.ac.il>
2021-08-29 20:29:27 +0300

    Updated register.js

--- Petstaurant/wwwroot/js/register.js
+++ Petstaurant/wwwroot/js/register.js
@@ -1,15 +1,19 @@
 ﻿$(function () {
     $('form').submit(function (e) {
         var f = $('#bDate').val();
-        console.log(f);
         var currentYear = new Date().getFullYear();
-        var msg = $('#error_message');
+        var msg = $('#bDateMsg');
         var year = f.split('-');
-        console.log(year[0] > currentYear);
-        console.log(year[0] < currentYear);
         if (currentYear-14 < year[0] || year[0] < currentYear - 120) {
             e.preventDefault();
             msg.text('You must be older than 14 or you enter invalid year');
+        }
+
+        var em = $('#email').val();
+        var emE = $('#emailError');
+        if (!em.endsWith(".com")) {
+            e.preventDefault();
+            emE.text('Email Address must belong to .com')
         }
     });
 });
