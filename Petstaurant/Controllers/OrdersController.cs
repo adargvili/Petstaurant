1dd6ec09c28b2c8c2f9eb1fb6582ae034aa0b62d
adargvili <adar555@gmail.com>
2021-09-08 16:46:32 +0300

    updated order model

--- Petstaurant/Controllers/OrdersController.cs
+++ Petstaurant/Controllers/OrdersController.cs
@@ -25,7 +25,7 @@ namespace Petstaurant.Controllers
         [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Index()
         {
-            var petstaurantContext = _context.Order.Include(o => o.Cart).Include(o => o.User);
+            var petstaurantContext = _context.Order.Include(o => o.Store).Include(o => o.User);
             return View(await petstaurantContext.ToListAsync());
         }
 
@@ -39,7 +39,7 @@ namespace Petstaurant.Controllers
             }
 
             var order = await _context.Order
-                .Include(o => o.Cart)
+                .Include(o => o.Store)
                 .Include(o => o.User)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (order == null)
@@ -60,7 +60,7 @@ namespace Petstaurant.Controllers
         [Authorize(Roles = "Customer")]
         public IActionResult Create()
         {
-            ViewData["CartId"] = new SelectList(_context.Cart, nameof(Cart.Id), nameof(Cart.Id));
+            ViewData["StoreId"] = new SelectList(_context.Store, nameof(Store.Id), nameof(Store.Address));
             ViewData["UserName"] = new SelectList(_context.User, nameof(Models.User.UserName), nameof(Models.User.UserName));
             return View();
         }
@@ -71,16 +71,43 @@ namespace Petstaurant.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = "Customer")]
-        public async Task<IActionResult> Create([Bind("Id,Country,Address,PostalCode,PhoneNumber,TotalPrice,Delivery,UserName,CartId")] Order order)
+        public async Task<IActionResult> Create([Bind("Id,CreditCard,StoreId,Address,PostalCode,PhoneNumber,TotalPrice,UserName")] Order order)
         {
 
             if (ModelState.IsValid)
             {
+                //var u = GetCurrentUserName();
+                //var cart = await _context.Cart.FirstOrDefaultAsync(s => s.UserName == u);
+                //var cartitems = _context.CartItem.ToList().Where(p => p.CartId == cart.Id);
+                //if (cartitems.Count() == 0 || cart.TotalPrice == 0)
+                //{
+                //    ViewData["Error"] = "Your cart is empty; cannot make your order.";
+                //    return View(order);
+                //}
+                //order.UserName = u;
+                //order.CartId = cart.Id;
+                //order.TotalPrice = cart.TotalPrice;
+                //foreach (CartItem cartitem in cartitems)
+                //{
+                //    if (cartitem != null)
+                //    {
+                //        OrderItem orderitem = new OrderItem();
+                //        orderitem.DishId = cartitem.DishId;
+                //        orderitem.Price = cartitem.Price;
+                //        orderitem.Quantity = cartitem.Quantity;
+                //        if (order.OrderItems == null)
+                //        {
+                //            order.OrderItems = new List<OrderItem>();
+                //        }
+                //        order.OrderItems.Add(orderitem);
+                //    }
+                //}
                 _context.Add(order);
+             //   await DeleteCartAfterOrder(cart.Id);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["CartId"] = new SelectList(_context.Cart, nameof(Cart.Id), nameof(Cart.Id), order.CartId);
+            ViewData["StoreId"] = new SelectList(_context.Store, "Id", "PostalCode", order.StoreId);
             ViewData["UserName"] = new SelectList(_context.User, nameof(Models.User.UserName), nameof(Models.User.UserName), order.UserName);
             return View(order);
         }
@@ -99,7 +126,7 @@ namespace Petstaurant.Controllers
             {
                 return NotFound();
             }
-            ViewData["CartId"] = new SelectList(_context.Cart, nameof(Cart.Id), nameof(Cart.Id), order.CartId);
+            ViewData["StoreId"] = new SelectList(_context.Store, "Id", "PostalCode", order.StoreId);
             ViewData["UserName"] = new SelectList(_context.User, nameof(Models.User.UserName), nameof(Models.User.UserName), order.UserName);
             return View(order);
         }
@@ -110,7 +137,7 @@ namespace Petstaurant.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Country,Address,PostalCode,PhoneNumber,TotalPrice,Delivery,UserName,CartId,OrderTime")] Order order)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CreditCard,StoreId,Address,PostalCode,PhoneNumber,TotalPrice,UserName,OrderTime")] Order order)
         {
             if (id != order.Id)
             {
@@ -137,7 +164,7 @@ namespace Petstaurant.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["CartId"] = new SelectList(_context.Cart, nameof(Cart.Id), nameof(Cart.Id), order.CartId);
+            ViewData["StoreId"] = new SelectList(_context.Store, "Id", "PostalCode", order.StoreId);
             ViewData["UserName"] = new SelectList(_context.User, nameof(Models.User.UserName), nameof(Models.User.UserName), order.UserName);
             return View(order);
         }
@@ -152,7 +179,7 @@ namespace Petstaurant.Controllers
             }
 
             var order = await _context.Order
-                .Include(o => o.Cart)
+                .Include(o => o.Store)
                 .Include(o => o.User)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (order == null)
@@ -196,5 +223,22 @@ namespace Petstaurant.Controllers
         {
             return _context.Order.Any(e => e.Id == id);
         }
-    }
+
+
+        	
+        [Authorize(Roles = "Customer")]
+        public async Task DeleteCartAfterOrder(int id)
+        {
+            var u = GetCurrentUserName();
+            var user = _context.User.FirstOrDefault(c => c.UserName == u);
+            var cartToDelete = _context.Cart.FirstOrDefault(c => c.UserName == u);
+            if (id != cartToDelete.Id || cartToDelete == null || user == null)
+	
+            {
+                return;
+            }
+            _context.Cart.Remove(cartToDelete);
+            user.Cart = new Cart();
+        }
+}
 }
--- Petstaurant/Models/Order.cs
+++ Petstaurant/Models/Order.cs
@@ -8,48 +8,50 @@ using System.Threading.Tasks;
 
 namespace Petstaurant.Models
 {
-    public enum Delivery
-    {
-        Standard,
-        Express
-    }
     public class Order
     {
         public int Id { get; set; }
         [Required]
-        [StringLength(30, MinimumLength = 2, ErrorMessage = "You are allowed to use only 2-30 characters")]
-        [RegularExpression(@"^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$", ErrorMessage =
-           "Country should only contain word characters, hyphens, spaces and apostrophes")]
-        public string Country { get; set; }
+        [DisplayName("Credit Card")]
+        [StringLength(16, MinimumLength = 16, ErrorMessage = "You are allowed to use only 16 characters")]
+        [RegularExpression(@"^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$", ErrorMessage =
+           "Credit card format is required")]
+        public string CreditCard { get; set; }
+        [DisplayName("Store")]
+        public int StoreId { get; set; }
         [Required]
-        [StringLength(30, MinimumLength = 5, ErrorMessage = "You are allowed to use only 5-30 characters")]
-        [RegularExpression(@"^[#.0-9a-zA-Z\s,-]+$", ErrorMessage =
-           "Please do not use specialized sybmols in the address")]
+        public Store Store { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "You are allowed to use only 5-50 characters")]
+        [RegularExpression(@"^[a-zA-Z0-9, ]*$", ErrorMessage =
+           "Israeli address format is required")]
         public string Address { get; set; }
         [Required]
         [DisplayName("Postal Code")]
-        [StringLength(30, MinimumLength = 4, ErrorMessage = "You are allowed to use only 4-30 characters")]
+        [RegularExpression(@"^\d{7}$", ErrorMessage =
+           "Israeli postal code format is required")]
+        [StringLength(7, MinimumLength = 7, ErrorMessage = "You are allowed to use only 7 digits")]
         [DataType(DataType.PostalCode)]
         public string PostalCode { get; set; }
         [Required]
         [DataType(DataType.PhoneNumber)]
-        [DisplayName("Phone Number")]
-        [StringLength(12, MinimumLength = 9, ErrorMessage = "You are allowed to use only 9-12 numbers")]
+        [DisplayName("Mobile Phone Number")]
+        [RegularExpression(@"^0(?:[234689]|5[0-689]|7[246789])(?![01])(\d{7})$", ErrorMessage =
+           "Israeli phone number format is required")]
+        [StringLength(10, MinimumLength = 9, ErrorMessage = "You are allowed to use only 9-10 numbers")]
         public string PhoneNumber { get; set; }
         [DataType(DataType.Currency)]
         [DisplayName("Total Price")]
-        [Range(0, 10000, ErrorMessage = "Choose a postive price")]
+        [Range(0, 10000, ErrorMessage = "Only a postive price, not more than 10000")]
         public double TotalPrice { get; set; }
-        public Delivery Delivery { get; set; } = Delivery.Standard;
-        [DisplayName("User")]
         [ForeignKey("User")]
         public string UserName { get; set; }
         public User User { get; set; }
         [DisplayName("Order Items")]
         public List<OrderItem> OrderItems { get; set; }
-        public int CartId { get; set; }
-        public Cart Cart { get; set; }
         [DataType(DataType.Date)]
         public DateTime OrderTime { get; set; } = DateTime.Now;
+
     }
 }
