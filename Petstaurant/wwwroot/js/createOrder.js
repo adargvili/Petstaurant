5f6706e9f2ea7bd9b12bcc0ac9f6597adf8fdc39
adargvili <adar555@gmail.com>
2021-09-09 13:31:43 +0300

    started createOrder.js

---
+++ Petstaurant/wwwroot/js/createOrder.js
@@ -0,0 +1,26 @@
+﻿$(function () {
+    $('#orderValidation').on("submit", function (e) {
+        var ad = $('#address').val();
+        var adEr = $('#addressError');
+        var c = false;
+        var a = ad.split(" ");
+        for (i in a) {
+            if (/^(?=.*[A-Z])(?=.*[0-9])[A-Z0-9]+$/.test(a[i])) {
+                c = true;
+                    break;
+            }
+        }
+        if (c||ad.startsWith(" ") || ad.endsWith(" ") || isNaN(ad.slice(-1) || !(ad.indexOf(',') > -1) || parseInt(ad) || !(/\d/.test(ad)) || ad.search(/\s/) > 5 )) {
+            e.preventDefault();
+            adEr.text('Israeli address format is required');
+        }
+        c = false;
+
+        var po = $('#postalCode').val();
+        var poE = $('#postalCodeError');
+        if (po.startsWith("0")) {
+            e.preventDefault();
+            poE.text('Israeli postal code format is required');
+        }
+    });
+});
