139b51ab83336c27c91b925d24257a77bd1e7854
adargvili <adar555@gmail.com>
2021-09-09 15:39:57 +0300

    orders updated

--- Petstaurant/Controllers/OrderItemsController.cs
+++ Petstaurant/Controllers/OrderItemsController.cs
@@ -11,7 +11,7 @@ using Petstaurant.Models;
 
 namespace Petstaurant.Controllers
 {
-    [Authorize(Roles = "Customer,Admin")]
+    [Authorize(Roles = "Admin")]
     public class OrderItemsController : Controller
     {
         private readonly PetstaurantContext _context;
@@ -21,148 +21,15 @@ namespace Petstaurant.Controllers
             _context = context;
         }
 
-        //// GET: OrderItems
-        //public async Task<IActionResult> Index()
-        //{
-        //    var petstaurantContext = _context.OrderItem.Include(o => o.Dish).Include(o => o.Order);
-        //    return View(await petstaurantContext.ToListAsync());
-        //}
-
-        //// GET: OrderItems/Details/5
-        //public async Task<IActionResult> Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var orderItem = await _context.OrderItem
-        //        .Include(o => o.Dish)
-        //        .Include(o => o.Order)
-        //        .FirstOrDefaultAsync(m => m.Id == id);
-        //    if (orderItem == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(orderItem);
-        //}
-
-        //// GET: OrderItems/Create
-        //public IActionResult Create()
-        //{
-        //    ViewData["DishId"] = new SelectList(_context.Dish, nameof(Dish.Id), nameof(Dish.Name));
-        //    ViewData["OrderId"] = new SelectList(_context.Order, nameof(Order.Id), nameof(Order.Address));
-        //    return View();
-        //}
-
-        //// POST: OrderItems/Create
-        //// To protect from overposting attacks, enable the specific properties you want to bind to.
-        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create([Bind("Id,OrderId,Quantity,Price,DishId")] OrderItem orderItem)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _context.Add(orderItem);
-        //        await _context.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    ViewData["DishId"] = new SelectList(_context.Dish, nameof(Dish.Id), nameof(Dish.Name), orderItem.DishId);
-        //    ViewData["OrderId"] = new SelectList(_context.Order, nameof(Order.Id), nameof(Order.Address), orderItem.OrderId);
-        //    return View(orderItem);
-        //}
-
-        //// GET: OrderItems/Edit/5
-        //public async Task<IActionResult> Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var orderItem = await _context.OrderItem.FindAsync(id);
-        //    if (orderItem == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    ViewData["DishId"] = new SelectList(_context.Dish, nameof(Dish.Id), nameof(Dish.Name), orderItem.DishId);
-        //    ViewData["OrderId"] = new SelectList(_context.Order, nameof(Order.Id), nameof(Order.Address), orderItem.OrderId);
-        //    return View(orderItem);
-        //}
-
-        //// POST: OrderItems/Edit/5
-        //// To protect from overposting attacks, enable the specific properties you want to bind to.
-        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("Id,OrderId,Quantity,Price,DishId")] OrderItem orderItem)
-        //{
-        //    if (id != orderItem.Id)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(orderItem);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!OrderItemExists(orderItem.Id))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    ViewData["DishId"] = new SelectList(_context.Dish, nameof(Dish.Id), nameof(Dish.Name), orderItem.DishId);
-        //    ViewData["OrderId"] = new SelectList(_context.Order, nameof(Order.Id), nameof(Order.Address), orderItem.OrderId);
-        //    return View(orderItem);
-        //}
-
-        //// GET: OrderItems/Delete/5
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var orderItem = await _context.OrderItem
-        //        .Include(o => o.Dish)
-        //        .Include(o => o.Order)
-        //        .FirstOrDefaultAsync(m => m.Id == id);
-        //    if (orderItem == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(orderItem);
-        //}
-
-        //// POST: OrderItems/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    var orderItem = await _context.OrderItem.FindAsync(id);
-        //    _context.OrderItem.Remove(orderItem);
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-        //}
-
-        //private bool OrderItemExists(int id)
-        //{
-        //    return _context.OrderItem.Any(e => e.Id == id);
-        //}
+        // GET: OrderItems
+        public async Task<IActionResult> Index()
+        {
+            var petstaurantContext = _context.OrderItem.Include(o => o.Dish).Include(o => o.Order);
+            return View(await petstaurantContext.ToListAsync());
+        }
+        private bool OrderItemExists(int id)
+        {
+            return _context.OrderItem.Any(e => e.Id == id);
+        }
     }
 }
