eb51391d8ef5c4f4dda24fcdfb023a56dba67b84
adargvili <adar555@gmail.com>
2021-09-10 09:34:19 +0300

    createOrder.js update

--- Petstaurant/wwwroot/js/createOrder.js
+++ Petstaurant/wwwroot/js/createOrder.js
@@ -6,18 +6,21 @@
         var a = ad.split(" ");
         if (a.length < 2 || a.length>4) {
             c = true;
+            console.log("test 1")
         }
         for (var i = 0; i < a.length; i++) {
             if (/^(?=.*[A-Z])(?=.*[0-9])[A-Z0-9]+$/.test(a[i])) {
                 c = true;
+                console.log("test 2")
                     break;
             }
         }
-        if (parseInt(a[0]) || !parseInt(a[a.length - 1]) || (ad.search(/\s/) > a.length-1)) {
+        if (parseInt(a[0]) || !parseInt(a[a.length - 1]) || (ad.match(/([\s]+)/g).length > a.length-1)) {
             c = true;
+            console.log((ad.search(/\s/) > a.length - 1))
         }
 
-        if (c || ad.startsWith(" ") || ad.endsWith(" ") || isNaN(ad.slice(-1) || ad.includes(",") || !ad.includes(" ")|| parseInt(ad) || !(/\d/.test(ad)) || ad.search(/\s/) > 4)) {
+    if (c || ad.startsWith(" ") || ad.endsWith(" ") || isNaN(ad.slice(-1) || ad.includes(",") || !ad.includes(" ") || parseInt(ad) || !(/\d/.test(ad)) || ad.match(/([\s]+)/g).length > 4)) {
             e.preventDefault();
             adEr.text('Israeli address format is required (Example: Israel Galili 5)');
         }
