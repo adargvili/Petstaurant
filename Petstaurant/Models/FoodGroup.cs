ff5299a868d64ebf89848e67ef95d9b614d1f248
adargvili <adar555@gmail.com>
2021-09-11 00:42:22 +0300

    added foodgroup annotations

--- Petstaurant/Models/FoodGroup.cs
+++ Petstaurant/Models/FoodGroup.cs
@@ -12,7 +12,7 @@ namespace Petstaurant.Models
         public int Id { get; set; }
         [Required]
         [RegularExpression(@"^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$", ErrorMessage =
-            "Name should only contain word characters, hyphens, spaces and apostrophes")]
+            "Please enter a valid food group name")]
         [StringLength(30, MinimumLength = 2, ErrorMessage = "You are allowed to use only 2-30 characters")]
         public string Name { get; set; }
         public List<Dish> Dishes { get; set; }
